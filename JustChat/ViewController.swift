5e6b22606925942c4397a2c58a862986b8710ab2
Alexander Saprykin <alex_afsoc@mail.ru>
2020-03-22 20:00:29 +0300

    Initial Commit

---
+++ JustChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JustChat
+//
+//  Created by Alexander Saprykin on 22.03.2020.
+//  Copyright © 2020 Alexander Saprykin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
